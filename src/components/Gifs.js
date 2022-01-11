e5e836e669f2ef222a1072486cb292719eef779c
Paul Okoye <Chibuzorokoye77@gmail.com>
2022-01-11 14:16:09 -0500

    Send help this API is killing me

---
+++ src/components/Gifs.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+import axios from "axios";
+
+
+class Gifs extends Component
+{
+    constructor()
+    {
+        this. api_key ="cqQ4dklzeTC1dDoo4yqF4GoYPb4vwH8E"
+    }
+}
