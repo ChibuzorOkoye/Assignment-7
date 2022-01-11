5e47b514b072736ddfef9129c4663df4a4d8bd15
Paul Okoye <Chibuzorokoye77@gmail.com>
2022-01-11 14:31:58 -0500

    Send help

--- src/components/Gifs.js
+++ src/components/Gifs.js
@@ -6,6 +6,15 @@ class Gifs extends Component
 {
     constructor()
     {
-        this. api_key ="cqQ4dklzeTC1dDoo4yqF4GoYPb4vwH8E"
+        
+    }
+
+
+
+
+    handleTrending()
+    {
+        const api_key ="cqQ4dklzeTC1dDoo4yqF4GoYPb4vwH8E"
+        const url = "http://api.giphy.com/v1/gifs/trending?api_key=" + api_key
     }
 }
