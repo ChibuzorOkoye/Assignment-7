01e5f1f2dc43b50088a30495e21172332711005d
Paul Okoye <Chibuzorokoye77@gmail.com>
2022-01-12 09:23:39 -0500

    Finshed structuring Search.js and App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,78 @@
 import logo from './logo.svg';
 import './App.css';
 import Gifs from "./components/Gifs"
-import Search from "./components/Search"
+import Search from "./components/Search.js"
+import { Component } from 'react';
 
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+  
+  constructor()
+    {
+        super()
+        this.state =
+        {
+          gifArr: [],
+          search: ""
+        }
+    }
+    handleTrending()
+    {
+        let limit = "&limit=25"
+        const api_key ="cqQ4dklzeTC1dDoo4yqF4GoYPb4vwH8E"
+        const url = "http://api.giphy.com/v1/gifs/trending?api_key=" + api_key
+
+         axios.get(url).then((reponse) =>
+        {
+            const getData = reponse.data.data;
+            console.log(getData);
+
+        }).catch((err) => console.log(err));
+
+        
+    }
+
+    handleReg()
+    {
+        let limit = "&limit=20"
+        const api_key ="cqQ4dklzeTC1dDoo4yqF4GoYPb4vwH8E"
+        const url = "http://api.giphy.com/v1/gifs/search?q=" + this.state.search + "&api_key=" + api_key + limit
+
+         axios.get(url).then((reponse) =>
+        {
+            const getData = reponse.data.data;
+            console.log(getData);
+
+        }).catch((err) => console.log(err));
+
+        
+    }
+
+    
+    handleRand()
+    {
+      let limit = "&limit=15"
+        const api_key ="cqQ4dklzeTC1dDoo4yqF4GoYPb4vwH8E"
+        const url = "http://api.giphy.com/v1/gifs/random?api_key=" + api_key
+
+         axios.get(url).then((reponse) =>
+        {
+            const getData = reponse.data.data;
+            console.log(getData);
+
+        }).catch((err) => console.log(err));
+
+        
+    }
+
+
+    handleChange(event) {
+        this.setState({ search: event.target.value});
+    }
+    render()
+    {
+      return(<Search search = {this.state.search} handleChange = {this.handleChange}/>)
+    }
 }
 
 export default App;
--- src/components/Search.js
+++ src/components/Search.js
@@ -2,60 +2,15 @@ import React, {Component} from "react";
 import axios from "axios";
 
 
-class Gifs extends Component
+const Search = ({search, handleChnage}) =>
 {
-    constructor()
-    {
-        super()
-
-        this.state = {
-            gifArr: []
-        }
-    }
-    handleTrending()
-    {
-        const api_key ="cqQ4dklzeTC1dDoo4yqF4GoYPb4vwH8E"
-        const url = "http://api.giphy.com/v1/gifs/trending?api_key=" + api_key
-
-         axios.get(url).then((reponse) =>
-        {
-            const getData = reponse.data;
-            console.log(getData);
-
-        }).catch((err) => console.log(err));
-
-        
-    }
-
-    handleReg(input)
-    {
-        const api_key ="cqQ4dklzeTC1dDoo4yqF4GoYPb4vwH8E"
-        const url = "http://api.giphy.com/v1/gifs/search?q=" + input + "&api_key=" + api_key
-
-         axios.get(url).then((reponse) =>
-        {
-            const getData = reponse.data;
-            console.log(getData);
-
-        }).catch((err) => console.log(err));
-
-        
-    }
-
-    handleReg()
-    {
-        const api_key ="cqQ4dklzeTC1dDoo4yqF4GoYPb4vwH8E"
-        const url = "http://api.giphy.com/v1/gifs/random?api_key=" + api_key
-
-         axios.get(url).then((reponse) =>
-        {
-            const getData = reponse.data;
-            console.log(getData);
-
-        }).catch((err) => console.log(err));
-
-        
-    }
-
-    
+    return
+        (
+                <div>
+                <input className='' type="text" placeholder="dogs, cats, etc.." ></input>
+                <input className='' type = "submit" value={search} onClick={this.handleReg()}></input>
+                <button className='' onClick={this.handleTrending()}>Trending</button>
+                <button className='' onClick={this.handleRand()}>Random</button>
+                </div> 
+        )
 }
